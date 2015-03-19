93ada99d3362d82e788916cb4f9198e58941781d
janocee <janet.cheung86@gmail.com>
2015-03-19 13:28:32 -0400

    Moved method from CategoryIconsHelper to ProfileHelper

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,4 +2,21 @@ module ProfileHelper
   def full_location(user)
     user.location.city && !user.location.city.empty? ? "#{user.location.city}, #{user.location.country}" : "#{user.location.country}"
   end
+
+  cat_img = [
+  "category-icons/Business & Career.png",
+  "category-icons/Current Events.png",
+  "category-icons/Education & Knowledge.png",
+  "category-icons/Electronics & Gadgets.png",
+  "category-icons/Entertainment.png",
+  "category-icons/Food.png",
+  "category-icons/Health & Fitness.png",
+  "category-icons/Money Matters.png",
+  "category-icons/Sports.png",
+  "category-icons/Travel & Lifestyle.png"
+]
+
+  def category_icon(id)
+    cat_img[id]
+  end
 end
