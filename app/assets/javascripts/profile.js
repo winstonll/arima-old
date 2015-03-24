10c1d83e35523e7aaa9131678399c929b03946f1
janocee <janet.cheung86@gmail.com>
2015-03-24 18:02:48 -0400

    More styling to the profile page

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,12 +3,20 @@
 
 $(document).ready(function() {
   $(".profile-questions-answered").click(function() {
+    $(".profile-questions-toggle-answered").addClass("active");
+    $(".profile-questions-toggle-asked").removeClass("active");
     $(".questions-answered").css("display", "block");
     $(".questions-asked").css("display", "none");
+    // $(".questions-asked").addClass("questions-hidden");
+    // $(".questions-answered").removeClass("questions-hidden");
   });
 
   $(".profile-questions-asked").click(function() {
+    $(".profile-questions-toggle-asked").addClass("active");
+    $(".profile-questions-toggle-answered").removeClass("active");
     $(".questions-asked").css("display", "block");
     $(".questions-answered").css("display", "none");
+    // $(".questions-answered").addClass("questions-hidden");
+    // $(".questions-asked").removeClass("questions-hidden");
   });
 });
