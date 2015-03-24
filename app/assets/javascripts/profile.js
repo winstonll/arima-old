ed346452a2416ccf0de4db9fdc2bd41d6804b81c
Jeff Martin <jffrymrtn1@gmail.com>
2015-03-24 19:26:33 -0400

    Commit code just in case

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,22 +1,17 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
 $(document).ready(function() {
   $(".profile-questions-answered").click(function() {
     $(".profile-questions-toggle-answered").addClass("active");
     $(".profile-questions-toggle-asked").removeClass("active");
-    $(".questions-answered").css("display", "block");
-    $(".questions-asked").css("display", "none");
-    // $(".questions-asked").addClass("questions-hidden");
-    // $(".questions-answered").removeClass("questions-hidden");
+
+    $(".questions-asked").addClass("questions-hidden");
+    $(".questions-answered").removeClass("questions-hidden");
   });
 
   $(".profile-questions-asked").click(function() {
     $(".profile-questions-toggle-asked").addClass("active");
     $(".profile-questions-toggle-answered").removeClass("active");
-    $(".questions-asked").css("display", "block");
-    $(".questions-answered").css("display", "none");
-    // $(".questions-answered").addClass("questions-hidden");
-    // $(".questions-asked").removeClass("questions-hidden");
+
+    $(".questions-answered").addClass("questions-hidden");
+    $(".questions-asked").removeClass("questions-hidden");
   });
 });
