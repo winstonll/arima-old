c8d6892e51aec840f241b5c1e1b39a5c9da28e3d
Jeff Martin <jffrymrtn1@gmail.com>
2015-03-24 15:15:26 -0400

    Help with some styling and css toggles

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,13 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready( function() {
-  $(".category_select").on("change", function() {
-    $.ajax({
-      url: "/profile",
-      type: "GET",
-      dataType: "script",
-      data: { category_type: $(".category_select").val() }
-    });
+$(document).ready(function() {
+  $(".profile-questions-answered").click(function() {
+    $(".questions-answered").css("display", "block");
+    $(".questions-asked").css("display", "none");
+  });
+
+  $(".profile-questions-asked").click(function() {
+    $(".questions-asked").css("display", "block");
+    $(".questions-answered").css("display", "none");
   });
 });
