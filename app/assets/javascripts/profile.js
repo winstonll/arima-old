d01afbbd31948f1c1d14aae9da62a965a2782a06
janocee <janet.cheung86@gmail.com>
2015-03-26 15:30:52 -0400

    Questions asked and questions answered toggles with the correct colours

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   $(".profile-questions-answered").click(function() {
-    $(".profile-questions-toggle-answered").addClass("active").css({"border-color": "#639ee3", "border-width": "2px", "border-style": "solid"});
-    $(".profile-questions-toggle-asked").removeClass("active").css({"border-color": "#9B9A9C", "border-width": "2px", "border-style": "solid"});
+    $(".profile-questions-toggle-answered").addClass("active").css({"color": "#639ee3", "border-color": "#639ee3", "border-width": "2px", "border-style": "solid"});
+    $(".profile-questions-toggle-asked").removeClass("active").css({"color": "#9B9A9C", "border-color": "#9B9A9C", "border-width": "2px", "border-style": "solid"});
 
     $(".questions-asked").addClass("questions-hidden");
     $(".questions-answered").removeClass("questions-hidden");
   });
 
   $(".profile-questions-asked").click(function() {
-    $(".profile-questions-toggle-asked").addClass("active").css({"border-color": "#639ee3", "border-width": "2px", "border-style": "solid"});
-    $(".profile-questions-toggle-answered").removeClass("active").css({"border-color": "#9B9A9C", "border-width": "2px", "border-style": "solid"});
+    $(".profile-questions-toggle-asked").addClass("active").css({"color": "#639ee3", "border-color": "#639ee3", "border-width": "2px", "border-style": "solid"});
+    $(".profile-questions-toggle-answered").removeClass("active").css({"color": "#9B9A9C", "border-color": "#9B9A9C", "border-width": "2px", "border-style": "solid"});
 
     $(".questions-answered").addClass("questions-hidden");
     $(".questions-asked").removeClass("questions-hidden");
