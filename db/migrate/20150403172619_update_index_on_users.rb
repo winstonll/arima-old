378193473d9044daa6c364627d08ed921ff1534c
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-04-03 13:40:02 -0400

    Revert "migration added for removing index on user emails"
    This reverts commit 7af5898b25e2aaeb933212cca728e5a34a870322.

--- db/migrate/20150403172619_update_index_on_users.rb
+++
@@ -1,7 +0,0 @@
-class UpdateIndexOnUsers < ActiveRecord::Migration
-  def up
-    sql = 'DROP INDEX index_users_on_email'
-    sql << ' ON users' if Rails.env == 'production' # Heroku pg
-    ActiveRecord::Base.connection.execute(sql)
-  end
-end
