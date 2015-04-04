4c6b2f145c14778fb9f3b57627d68f6365dac7f9
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-04-03 20:01:58 -0400

    ran migration and removed index on email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403042054) do
+ActiveRecord::Schema.define(version: 20150403210817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,7 +159,6 @@ ActiveRecord::Schema.define(version: 20150403042054) do
     t.string   "ip_address"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
