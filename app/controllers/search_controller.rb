4ca9a9f6360778d5cba1e3847fe2f4a0fde2993a
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-04-25 14:30:50 -0400

    stylization for the search results page and function for the search task is now finished.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,18 +10,24 @@ class SearchController < ApplicationController
     stripped -= %w{for and nor but or yet so either not only may neither both whether just as much rather why the is a this then than them their}
     stripped = stripped.join(" ")
 
-    @result = Array.new()
+    @all = Array.new()
     r = Regexp.new(Regexp.escape(data.downcase))
 
     unless(stripped.empty?)
       Question.find_each do |question|
         unless (r.match(question.label.downcase).to_s.empty?)
-          @result << question
+          @all << question
         end
       end
     end
 
-    if(@result.empty?)
+    if @user
+      user_questions = @user.questions
+      @answered         = @all & user_questions
+      @unanswered       = @all - @answered
+    end
+
+    if(@all.empty?)
       flash[:notice] = "Nothing matched your search."
     end
   end
