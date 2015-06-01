e3688f8a261fa7c83a06ee5acf26a4543b83097e
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-06-01 12:12:35 -0400

    quick sorting the leaderboard to see if this does not give bad gateway

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -8,15 +8,23 @@ class LeaderboardsController < ApplicationController
     @countries = Location.select(:country_code).distinct.collect { |loc| loc.country_code }
 
     # Use as a base for location filtering and user rank
-    #@registered_users = User.get_ranked_users
+    @registered_users = Array.new
+
+    User.find_each do |user|
+      @registered_users << user.points
+    end
+
+    @registered_users = qsort(@registered_users)
+
+    rank = @registered_users.length - @registered_users.find_index { |w| w == 2 }
 
     #if @countries.find { |c| c == params[:country_type] }
     #  @ranked_registered_users = @registered_users.where('locations.country_code' => params[:country_type])[0...50]
     #else
       @ranked_registered_users = User.order("points DESC").limit(50)
     #end
-    #user_signed_in? ? current_user.get_user_rank :
-	  @user_rank =  "?"
+
+	  @user_rank =  user_signed_in? ? rank : "?"
 	  @user = current_user
 
 	  #respond_to do |format|
