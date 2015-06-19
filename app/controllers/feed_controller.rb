8bcb3653bc810da5759dfee81d50faa4010d9bc2
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-06-18 22:55:34 -0400

    adding chosen group icon in sidebar for return purposes

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -8,10 +8,12 @@ class FeedController < ApplicationController
     if(cookies[:guest] == nil)
       check_guest()
     end
+    cookies[:group_id] = nil
     @all = Question.all.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def category
+    cookies[:group_id] = params[:group_id]
     @all = Question.all.where(group_id: params[:group_id]).page(params[:page]).per(15)
     respond_to do |format|
       format.html { render :template => "feed/index" }
