5e87ddfdd665125611875532722690a7ecf30c3e
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-07-20 15:01:16 -0400

    adding the code i reverted previously

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -176,6 +176,12 @@ class QuestionsController < ApplicationController
       return
     end
 
+    if params[:submit_question_name].empty?
+      redirect_to :back
+      flash[:notice] = "Please enter a question label/description"
+      return
+    end
+
     # For Multiple Choice Questions, concatenate the answer boxes into
     # one string, checking for empty boxes and removing them
     13.times do |count|
