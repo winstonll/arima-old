eb2d4526e133c45a0fd84f02c48a943743854e07
Ruben Avila <Ruavilaa@hotmail.com>
2015-10-27 14:33:30 -0400

    removed answerplus from shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027181426) do
+ActiveRecord::Schema.define(version: 20151027182324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,6 @@ ActiveRecord::Schema.define(version: 20151027181426) do
     t.integer  "user_id"
     t.integer  "group_id"
     t.integer  "votecount",              default: 0
-    t.boolean  "answer_plus"
     t.string   "image_link"
     t.boolean  "shared_image"
     t.integer  "tag_count",              default: 0
